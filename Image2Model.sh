7932ec51cc56c4f0a4095ba2845812ebc51bd1aa
stefanovilla-ak <stefano.villa@animoka.com>
2025-12-09 17:20:25 +0100

    create sh for testing

---
+++ Image2Model.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+source /home/stefano/miniconda3/envs/sam3d-objects/bin/activate
+python image_to_model -i notebook/images/shutterstock_stylish_kidsroom_1640806567/image.png -m notebook/images/shutterstock_stylish_kidsroom_1640806567/14.png -o output2
